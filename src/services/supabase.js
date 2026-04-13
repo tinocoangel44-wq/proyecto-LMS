b1d3f5f9f06af1adc62bfac28eb40709bb5881dc
Silverio <tinocoangel44@gmail.com>
2026-04-12 20:35:19 -0600

    Fix syntax errors manually introduced and warnings

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = REACT_APP_SUPABASE_URL || 'https://kzrfxoxiizedyaacngto.supabase.co';
-const supabaseAnonKey = REACT_APP_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt6cmZ4b3hpaXplZHlhYWNuZ3RvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1MzU0ODUsImV4cCI6MjA5MTExMTQ4NX0.gxSeJjwg2HD8uA9ifyw47EYiMUfhy0F-rCIH9aMvpUQ';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || 'https://kzrfxoxiizedyaacngto.supabase.co';
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt6cmZ4b3hpaXplZHlhYWNuZ3RvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1MzU0ODUsImV4cCI6MjA5MTExMTQ4NX0.gxSeJjwg2HD8uA9ifyw47EYiMUfhy0F-rCIH9aMvpUQ';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
